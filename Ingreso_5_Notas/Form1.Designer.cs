ad3b0e75e6ead75b047fab15223731e45bcf9053
Michael Escobar Galeano <maicolgemelo@gmail.com>
2019-09-15 11:43:49 -0500

    ejercicio el cual el usuario ingresa 5 notas y se le saca el promedio y la clasificacion de rendimiento

---
+++ Ingreso_5_Notas/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Ingreso_5_Notas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.txtNota4 = new System.Windows.Forms.TextBox();
+            this.txtNota5 = new System.Windows.Forms.TextBox();
+            this.lblNota1 = new System.Windows.Forms.Label();
+            this.lblNota2 = new System.Windows.Forms.Label();
+            this.lblNota3 = new System.Windows.Forms.Label();
+            this.lblNota4 = new System.Windows.Forms.Label();
+            this.lblNota5 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota1.Location = new System.Drawing.Point(215, 40);
+            this.txtNota1.MaxLength = 3;
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(100, 25);
+            this.txtNota1.TabIndex = 0;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota2.Location = new System.Drawing.Point(215, 75);
+            this.txtNota2.MaxLength = 3;
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(100, 25);
+            this.txtNota2.TabIndex = 1;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota3.Location = new System.Drawing.Point(215, 114);
+            this.txtNota3.MaxLength = 3;
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(100, 25);
+            this.txtNota3.TabIndex = 2;
+            // 
+            // txtNota4
+            // 
+            this.txtNota4.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota4.Location = new System.Drawing.Point(215, 151);
+            this.txtNota4.MaxLength = 3;
+            this.txtNota4.Name = "txtNota4";
+            this.txtNota4.Size = new System.Drawing.Size(100, 25);
+            this.txtNota4.TabIndex = 3;
+            // 
+            // txtNota5
+            // 
+            this.txtNota5.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota5.Location = new System.Drawing.Point(215, 189);
+            this.txtNota5.MaxLength = 3;
+            this.txtNota5.Name = "txtNota5";
+            this.txtNota5.Size = new System.Drawing.Size(100, 25);
+            this.txtNota5.TabIndex = 4;
+            // 
+            // lblNota1
+            // 
+            this.lblNota1.AutoSize = true;
+            this.lblNota1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota1.Location = new System.Drawing.Point(83, 41);
+            this.lblNota1.Name = "lblNota1";
+            this.lblNota1.Size = new System.Drawing.Size(126, 20);
+            this.lblNota1.TabIndex = 5;
+            this.lblNota1.Text = "Ingrese Nota 1 : ";
+            // 
+            // lblNota2
+            // 
+            this.lblNota2.AutoSize = true;
+            this.lblNota2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota2.Location = new System.Drawing.Point(83, 80);
+            this.lblNota2.Name = "lblNota2";
+            this.lblNota2.Size = new System.Drawing.Size(126, 20);
+            this.lblNota2.TabIndex = 6;
+            this.lblNota2.Text = "Ingrese Nota 2 : ";
+            // 
+            // lblNota3
+            // 
+            this.lblNota3.AutoSize = true;
+            this.lblNota3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota3.Location = new System.Drawing.Point(83, 119);
+            this.lblNota3.Name = "lblNota3";
+            this.lblNota3.Size = new System.Drawing.Size(122, 20);
+            this.lblNota3.TabIndex = 7;
+            this.lblNota3.Text = "Ingrese Nota 3 :";
+            // 
+            // lblNota4
+            // 
+            this.lblNota4.AutoSize = true;
+            this.lblNota4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota4.Location = new System.Drawing.Point(83, 156);
+            this.lblNota4.Name = "lblNota4";
+            this.lblNota4.Size = new System.Drawing.Size(122, 20);
+            this.lblNota4.TabIndex = 8;
+            this.lblNota4.Text = "Ingrese Nota 4 :";
+            // 
+            // lblNota5
+            // 
+            this.lblNota5.AutoSize = true;
+            this.lblNota5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota5.Location = new System.Drawing.Point(83, 194);
+            this.lblNota5.Name = "lblNota5";
+            this.lblNota5.Size = new System.Drawing.Size(122, 20);
+            this.lblNota5.TabIndex = 9;
+            this.lblNota5.Text = "Ingrese Nota 5 :";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(511, 59);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(141, 31);
+            this.btnCalcular.TabIndex = 10;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuesta.Location = new System.Drawing.Point(140, 317);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(87, 20);
+            this.lblRespuesta.TabIndex = 11;
+            this.lblRespuesta.Text = "Respuesta";
+            this.lblRespuesta.Visible = false;
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensaje.Location = new System.Drawing.Point(280, 317);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(69, 20);
+            this.lblMensaje.TabIndex = 12;
+            this.lblMensaje.Text = "Mensaje";
+            this.lblMensaje.Visible = false;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(511, 119);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(141, 32);
+            this.btnBorrar.TabIndex = 13;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblNota5);
+            this.Controls.Add(this.lblNota4);
+            this.Controls.Add(this.lblNota3);
+            this.Controls.Add(this.lblNota2);
+            this.Controls.Add(this.lblNota1);
+            this.Controls.Add(this.txtNota5);
+            this.Controls.Add(this.txtNota4);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.TextBox txtNota4;
+        private System.Windows.Forms.TextBox txtNota5;
+        private System.Windows.Forms.Label lblNota1;
+        private System.Windows.Forms.Label lblNota2;
+        private System.Windows.Forms.Label lblNota3;
+        private System.Windows.Forms.Label lblNota4;
+        private System.Windows.Forms.Label lblNota5;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Button btnBorrar;
+    }
+}
+
---
+++ Ingreso_5_Notas/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ingreso_5_Notas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string Nota1 = txtNota1.Text;
+            string Nota2 = txtNota2.Text;
+            string Nota3 = txtNota3.Text;
+            string Nota4 = txtNota4.Text;
+            string Nota5 = txtNota5.Text;
+            string promedio = lblRespuesta.Text;
+
+            float Calif1 = float.Parse(Nota1);
+            float Calif2 = float.Parse(Nota2);
+            float Calif3 = float.Parse(Nota3);
+            float Calif4 = float.Parse(Nota4);
+            float Calif5 = float.Parse(Nota5);
+            float prom = (Calif1 + Calif2 + Calif3 + Calif4 + Calif5)/5 ;
+
+
+            if (prom > 5)
+            {
+
+                lblRespuesta.Text = "";
+                lblMensaje.Text = "";
+                MessageBox.Show("puede que halla sobre pasado el limite de calificación o el simbolo no es punto(.) sino (,), por favor verifique el error ");
+
+
+
+            }
+            else if (prom <= 1)
+            {
+
+                lblRespuesta.Text = prom.ToString();
+                lblMensaje.Text = "lleba la materia sin lograr realizar proceso de recuperación ";
+                lblRespuesta.Visible = true;
+                lblMensaje.Visible = true;
+                lblMensaje.ForeColor = Color.Red;
+            }
+            else if (prom < 3)
+            {
+                lblRespuesta.Text = prom.ToString();
+                lblMensaje.Text = "Reprueba la materia y puede hacer la habilitación ";
+                lblRespuesta.Visible = true;
+                lblMensaje.Visible = true;
+                lblMensaje.ForeColor = Color.Blue;
+
+            }
+            else if (prom < 4)
+            {
+                lblRespuesta.Text = prom.ToString();
+                lblMensaje.Text = "Aprueba la materia con plan de mejora ";
+                lblRespuesta.Visible = true;
+                lblMensaje.Visible = true;
+                lblMensaje.ForeColor = Color.Green;
+
+
+
+            }
+            else if (prom <= 4.5)
+            {
+
+                lblRespuesta.Text = prom.ToString();
+                lblMensaje.Text = " buen Rendimiento ";
+                lblRespuesta.Visible = true;
+                lblMensaje.Visible = true;
+                lblMensaje.ForeColor = Color.Green;
+
+
+
+
+            }
+            else if (prom < 5)
+            {
+
+                lblRespuesta.Text = prom.ToString();
+                lblMensaje.Text = "Excelente ";
+                lblRespuesta.Visible = true;
+                lblMensaje.Visible = true;
+                lblMensaje.ForeColor = Color.Green;
+
+
+
+
+
+            }
+            else {
+                lblRespuesta.Text = prom.ToString();
+                lblMensaje.Text = "Graduado con honores ";
+                lblRespuesta.Visible = true;
+                lblMensaje.Visible = true;
+                lblMensaje.ForeColor = Color.Green;
+
+            }
+
+        }
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtNota1.Text = "";
+            txtNota2.Text = "";
+            txtNota3.Text = "";
+            txtNota4.Text = "";
+            txtNota5.Text = "";
+            lblMensaje.Text = "";
+            lblRespuesta.Text = "";
+        }
+
+
+    }
+
+
+}
